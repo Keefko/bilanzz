e45499f022d2754d7bcf963d04291155407e3db2
Keefko <kohooutdan@gmail.com>
2021-03-15 01:56:33 +0100

    secure assets

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('style.css') }}">
     <script type="text/javascript" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/gsap-latest-beta.min.js"></script>
     <script type="module" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </head>
@@ -22,6 +22,6 @@
 
 
 <div id="preloader"></div>
-<script type="text/javascript" src="{{asset('main.js')}}"></script>
+<script type="text/javascript" src="{{secure_asset('main.js')}}"></script>
 </body>
 </html>
